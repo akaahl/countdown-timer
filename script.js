6b75b35bd5081ef078df27894785b6529d538ebe
a.k.a.a.h.l <a.k.afiq@live.com>
2021-04-03 18:31:43 +0800

    Latest commit

--- script.js
+++ script.js
@@ -7,58 +7,22 @@ const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 
-function flipInterval() {
-  upperHalves.forEach(upperhalf => {
-    upperhalf.style.transform = `rotateX(-180deg)`;
-    upperhalf.style.transition = `all 1s linear`;
-  });
-
-  numbers.forEach(number => {
-    number.style.transform = `rotateX(-180deg)`;
-    number.style.transition = `all 1s linear`;
-  });
+function flipInterval(arg) {
+  let previousElement = arg.previousElementSibling.previousElementSibling;
+  previousElement.style.transform = `rotateX(-180deg)`;
+  previousElement.style.transition = `all 0.5s linear`;
+  previousElement.style.background;
 
   setTimeout(() => {
-    upperHalves.forEach(upperhalf => {
-      upperhalf.style.transform = `rotateX(0deg);`;
-    });
-
-    numbers.forEach(number => {
-      number.style.transform = `rotateX(0deg)`;
-    });
-  }, 1000);
+    previousElement.style.transform = `rotateX(0deg)`;
+    previousElement.style.transition = `all 0s`;
+  }, 500);
 }
-
-// setInterval(flipInterval, 1000);
-
-// Todo: fix CSS upper half and timer container background color
-
-function nextweek() {
-  var today = new Date();
-  var nextweek = new Date(
-    today.getFullYear(),
-    today.getMonth(),
-    today.getDate() + 8,
-    today.getHours() + 23,
-    today.getMinutes() + 55,
-    today.getSeconds() + 41
-  );
-
-  const diff = nextweek - today;
-  // const days = Math.floor(diff / 1000 / 60 / 60 / 24);
-
-  // const daysCountdown = today.getDate() + 8 - today.getDate();
-  // const hoursCountdown = today.getHours() + 23 - today.getHours();
-  // const minutesCountdown = today.getMinutes() + 55 - today.getMinutes();
-  // const secondsCountdown = today.getSeconds() + 14 - today.getSeconds();
-  console.log(typeof days.innerHTML);
-}
-
-nextweek();
 
 function timeUpdate(time) {
   let timeCountdown = (time.innerHTML -= 1);
   time.innerHTML = timeCountdown < 10 ? '0' + timeCountdown : timeCountdown;
+  setInterval(flipInterval(time), 500);
 }
 
 function updateCountdown() {
@@ -67,16 +31,15 @@ function updateCountdown() {
   if (seconds.innerHTML == '0-1') {
     seconds.innerHTML = '59';
     timeUpdate(minutes);
+  }
+  if (minutes.innerHTML == '0-1') {
+    minutes.innerHTML = '59';
+    timeUpdate(hours);
+  }
 
-    if (minutes.innerHTML == '0-1') {
-      minutes.innerHTML = '59';
-      timeUpdate(hours);
-
-      if (hours.innerHTML == '0-1') {
-        hours.innerHTML = '59';
-        timeUpdate(days);
-      }
-    }
+  if (hours.innerHTML == '0-1') {
+    hours.innerHTML = '59';
+    timeUpdate(days);
   }
 }
 
